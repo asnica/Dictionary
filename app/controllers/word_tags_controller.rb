49ad5a0f03b5a8d07df2cb077f36a5450ade48eb
JIYUNPARK <dbswlqkr00@gmail.com>
2026-01-23 00:51:24 +0900

    wordtag controller crud

--- app/controllers/word_tags_controller.rb
+++ app/controllers/word_tags_controller.rb
@@ -1,28 +1,67 @@
 class WordTagsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_word_tag, only: [:edit, :update, :destroy]
+  before_action :set_word_tag, only: [:show,:edit, :update, :destroy]
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
     @system_tags = WordTag.system_tags.order(:name)
-    @custom_tags = current_user.word_tags.order(created_at: :desc)
+    
+    if params[:search].present?
+       @custom_tags = current_user.word_tags
+                          .where("name ILIKE ?", "%#{params[:search]}%")
+                          .order(created_at: :desc)
+    else
+      @custom_tags = current_user.word_tags.order(created_at: :desc)
+    end
+
+                    
+  
+  
+  
   end
+
+  def show
+    @words = @word_tag.words.order(:japanese)
+  end
+
+
+ 
 
   def new
     @word_tag = WordTag.new
   end
 
   def create
+    @word_tag = current_user.word_tags.build(word_tag_params)
+    @word_tag.category = "custom"
+
+    if @word_tag.save
+      flash[:notice] = "タグが作成されました。"
+      redirect_to word_tags_path
+    else
+      flash.now[:alert] = "登録に失敗しました。"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @word_tag.update(word_tag_params)
+      flash[:notice] = "タグが更新されました。"
+      redirect_to word_tags_path
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @word_tag.destroy
+    flash[:notice] = "タグが削除されました。"
+    redirect_to word_tags_path
   end
 
 
