b73263505a535d8dbff2b3df890e359d96e03ac0
JIYUNPARK <dbswlqkr00@gmail.com>
2026-01-30 15:01:55 +0900

    add CSV function

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -70,14 +70,53 @@ class WordsController < ApplicationController
       redirect_to words_path
     end
 
+    def export_csv
+      @words = Word.all
 
+      if params[:search].present?
+        search_params = params[:search]
 
+        if search_params[:japanese].present?
+          @words = @words.where("japanese LIKE ?", "%#{search_params[:japanese]}%")
+        end
+        if search_params[:english].present?
+          @words = @words.where("english LIKE ?", "%#{search_params[:english]}%")
+        end
+
+        if search_params[:word_tag_id].present?
+          @words = @words.joins(:word_taggings).where(word_taggings: { word_tag_id: search_params[:word_tag_id] })
+        end
+
+      end
+
+      @words = @words.distinct.order(:japanese)
+
+      require "csv"
+      csv_data = CSV.generate(headers: true) do |csv|
+        csv << [ "日本語", "読み", "意味" ]
+        @words.each do |word|
+          csv << [ word.japanese, word.reading, word.english ]
+        end
+      end
+
+      send_data csv_data,
+        filename: "words_#{Time.current.strftime('%Y%m%d_%H%M%S')}.csv",
+        type: "text/csv; charset=UTF-8",
+        disposition: "attachment"
+    end
   private
   def set_word
     @word = Word.find(params[:id])
   end
 
   def word_params
-    params.require(:word).permit(:japanese, :english, :reading, :image, word_tag_ids: [], synonyms_attributes: [ :id, :synonym_word, :_destroy ])
+    params.require(:word).permit(
+      :japanese,
+      :reading,
+      :korean,
+      :image,
+      word_tag_ids: [],
+      synonyms_attributes: [ :id, :synonym_word, :_destroy ]
+    )
   end
 end
